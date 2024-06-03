1de1c16c0fd7a26b89220d7bcef822700e3c2614
thnomas <thombot43@gmail.com>
2024-06-03 10:53:35 +0100

    account route

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ import os
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY')  # Set secret key from environment variable
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
@@ -12,5 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from app.forms import LoginForm, RegistrationForm
 from app.models import User, Deck
 from app import app, db, bcrypt
-from flask_login import login_user, logout_user, current_user
+from flask_login import login_user, logout_user, login_required, current_user
 
 decks = [
     {
@@ -51,7 +51,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('index'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('index'))
         else:
             flash('Login Unsuccessful', 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -60,3 +61,9 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route("/account")
+@login_required
+def account():
+
+    return render_template('account.html', title='account')
